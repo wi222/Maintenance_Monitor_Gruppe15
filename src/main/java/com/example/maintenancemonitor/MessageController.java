e09fd9ebe9233565f8b4bd2e8445bc19057a9e15
Laurin Krobath <lkrobath@student.tgm.ac.at>
2023-07-03 20:28:27 +0200

    deleted wrong methods

--- src/main/java/com/example/maintenancemonitor/MessageController.java
+++ src/main/java/com/example/maintenancemonitor/MessageController.java
@@ -6,23 +6,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MessageController {
-
     private String message = "Everything works as expected";
 
     @GetMapping("/api/message")
     public String getMessage() {
         return message;
-    }
-
-    @GetMapping("/api/message/set")
-    public String setMessage(@RequestParam String m) {
-        this.message = m;
-        return "ok";
-    }
-
-    @GetMapping("/api/message/reset")
-    public String resetMessage() {
-        this.message = "Everything works as expected";
-        return "ok";
     }
 }
